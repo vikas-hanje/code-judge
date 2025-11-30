c8c9fec5f9da54279c3fe0b07ee31e3e6257b5f1
Vikas Hanje <153820746+vikas-hanje@users.noreply.github.com>
2025-11-30 21:36:33 +0530

    api removed

--- code-judge/.next/server/middleware-build-manifest.js
+++ code-judge/.next/server/middleware-build-manifest.js
@@ -9,6 +9,11 @@ self.__BUILD_MANIFEST = {
   "lowPriorityFiles": [],
   "rootMainFiles": [],
   "pages": {
+    "/": [
+      "static/chunks/webpack.js",
+      "static/chunks/main.js",
+      "static/chunks/pages/index.js"
+    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -19,25 +24,10 @@ self.__BUILD_MANIFEST = {
       "static/chunks/main.js",
       "static/chunks/pages/_error.js"
     ],
-    "/bughunter": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/bughunter.js"
-    ],
     "/problems": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/problems.js"
-    ],
-    "/problems/[slug]": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/problems/[slug].js"
-    ],
-    "/submissions": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/submissions.js"
     ]
   },
   "ampFirstPages": []
